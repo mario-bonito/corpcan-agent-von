396d344235077fe44bc19555528e63b670ee6232
Mario Bonito <mario.bonito@canada.ca>
2019-09-06 14:25:44 -0400

    tweaked textual form references and  JS

--- assets/js/gccorp.js
+++ assets/js/gccorp.js
@@ -1,21 +1,5 @@
 /* global FORM_HANDLERS, $ */
 
-FORM_HANDLERS['registration.von.canada.ca'] = function (form, response) {
-  console.log('here')
-  if (response.success === true) {
-    $(form).find('.legal-entity-id').remove()
-    $(form).append(
-      `
-      <div class="legal-entity-id">
-      Your legal entity id is:
-      <code>${response.result.claim.legal_entity_id[0]}</code>
-      </div>
-
-      `
-    )
-  }
-}
-
 (function($){
   var autocomplete_key = 'UP49-MX76-PT22-ZM49';
   $.typeahead({
